cfc7f596df5561b402984cc034c66bbb84d89bc6
Brad Kahl <bradkahl@Brads-MacBook-Air.local>
2025-02-27 15:50:50 -0500

    second commit, simple component, composite component, data and screens section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: false
+  selector: 'app-root',
+  standalone: false,
+  template: `
+    <app-inbox-screen></app-inbox-screen>
+  `,
 })
 export class AppComponent {
   title = 'taskbox';
