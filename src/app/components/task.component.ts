1dc52454438f53929679e9369e816e88de57d0bc
Brad Kahl <bradkahl@gmail.com>
2025-02-28 16:38:03 -0500

    add long title task example

--- src/app/components/task.component.ts
+++ src/app/components/task.component.ts
@@ -33,6 +33,7 @@ import { Task } from '../models/task.model';
           id="title-{{ task?.id }}"
           name="title-{{ task?.id }}"
           placeholder="Input title"
+         style="text-overflow: ellipsis;"
         />
       </label>
       <button
